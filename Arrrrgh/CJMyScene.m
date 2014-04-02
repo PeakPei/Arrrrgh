8a996df2534ef8d12a05097eb69d6049ce48f195
Shane Vitarana <shanev@gmail.com>
2014-04-02 12:04:47 -0700

    added rock asset

--- Arrrrgh/CJMyScene.m
+++ Arrrrgh/CJMyScene.m
@@ -15,9 +15,16 @@
         
         self.backgroundColor = [SKColor colorWithRed:0.16 green:0.65 blue:0.84 alpha:1.0];
         [self createShip];
+        [self createRock];
     }
     return self;
 }
+
+-(void)update:(CFTimeInterval)currentTime {
+    /* Called before each frame is rendered */
+}
+
+#pragma mark - UIResponder
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     for (UITouch *touch in touches) {
@@ -25,19 +32,22 @@
     }
 }
 
--(void)update:(CFTimeInterval)currentTime {
-    /* Called before each frame is rendered */
-}
+#pragma mark - Internal Methods
 
 - (void)createShip {
     SKSpriteNode *ship = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-    
     ship.position = CGPointMake(CGRectGetMidX(self.frame),
-                                CGRectGetMidY(self.frame) - 100.0);
-    
-    ship.size = CGSizeMake(50.0, 50.0);
-    
+                                CGRectGetMidY(self.frame) - 150.0);
+    ship.size = CGSizeMake(100.0, 100.0);
     [self addChild:ship];
+}
+
+- (void)createRock {
+    SKSpriteNode *rock = [SKSpriteNode spriteNodeWithImageNamed:@"rock"];
+    rock.position = CGPointMake(CGRectGetMidX(self.frame),
+                                CGRectGetMidY(self.frame));
+//    ship.size = CGSizeMake(50.0, 50.0);
+    [self addChild:rock];
 }
 
 @end
